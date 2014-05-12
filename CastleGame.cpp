b93179687559f0c200128e3de886f75cb7043393
DownToNuggen <dn6634@hotmail.com>
2014-05-11 22:33:25 -0400

    Update CastleGame.cpp

--- CastleGame.cpp
+++ CastleGame.cpp
@@ -319,40 +319,6 @@ areaSelection:
 
 }
 
-getMap()
-{
-                                                 
-    cout <<"                                                     N        " << endl;
-    cout <<"                                                     |        " << endl;
-    cout <<"                                                 W---+---E    " << endl;
-    cout <<"                                                     |        " << endl;
-    cout <<"                                                     S        " << endl;
-    cout <<"              {Base Stair}                                    " << endl;
-    cout <<"              {          }                                    " << endl;
-    cout <<"              {          }                                    " << endl;
-    cout <<"              ~~~~~  ~~~~~                                    " << endl;
-    cout <<"               ====  =====                         ========== " << endl;
-    cout <<"              |           |====================== |          |" << endl;
-    cout <<"              |           |           |           |          |" << endl;
-    cout <<"              |           |  Room1    |   Room3   |          |" << endl;
-    cout <<"              |           |   (1)     |   (3)     |          |" << endl;
-    cout <<"              |    @@@    |   Icer    |===== =====|          |" << endl;
-    cout <<"              |           |           |           |          |" << endl;
-    cout <<"              |   Lobby    ==== ======|   Study      Kitchen |" << endl;
-    cout <<"              |    (0)    First Hallway    Room       (6)    |" << endl;
-    cout <<"              |            ==== ======|     (4)              |" << endl;
-    cout <<"              |  Police   |           |   Alireza |          |" << endl;
-    cout <<"              |           |  Room2    |===== ==== |          |" << endl;
-    cout <<"              |           |   (2)     | BathRoom  |          |" << endl;
-    cout <<"              |           |   Dinh    |   Xin (5) |          |" << endl;
-    cout <<"               ==========  =========== ==========  ==========|" << endl;
-    cout <<"              {          }                                    " << endl;
-    cout <<"              {2nd  Stair}                                    " << endl;
-    cout <<"              {          }                                    " << endl;
-    cout <<"              ~~~~~~~~~~~~                                    " << endl;
-    
-}
-
 //choice(char playerChoice, int flag)
 //{
     
