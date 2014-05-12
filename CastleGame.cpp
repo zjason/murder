b4526e30f5def57d9c682c53c1663dd7518e0f48
zjason <zmr8471@hotmail.com>
2014-05-12 00:55:50 -0400

    Update CastleGame.cpp
    update image and functions

--- CastleGame.cpp
+++ CastleGame.cpp
@@ -1,15 +1,16 @@
-//STILL IN CONSTRUCTION DO NOT REVIEW OR ATTEMPT THIS GAME YET
+#include "CastleGame.h"
 
-int main(int argc, char *argv)
+
+int main()
 {
     // game name
     cout << endl;
     cout << " ___                 _    _                                     _              " << endl;
-    cout << "|  _`\              | |_ (_ |           /'\_/`\                | |             " << endl;
-    cout << "| | \_\   _ _   ___ | ,_) | |    __     |     | _   _  _ __   _| |   __   _ __ " << endl;
-    cout << "| |  _  /'_` )/',__)| |   | |  /'__`\   | (_) || | | || '__)/'_` | /'__`\| '__)" << endl;
-    cout << "| |_/ /( (_| |\__, \| |_  | |  | ___/   | | | || |_| || |  ( (_| | | ___/| |   " << endl;
-    cout << "|____/'`\__,_)(____/`\__)(___)`\____)   |_| |_|`\___/'|_|  `\__,_|`\____)|_|   " << endl;
+    cout << "|  _`\\              | |_ (_ |           /'\\_/`\\                | |             " << endl;
+    cout << "| | \\_\\   _ _   ___ | ,_) | |    __     |     | _   _  _ __   _| |   __   _ __ " << endl;
+    cout << "| |  _  /'_` )/',__)| |   | |  /'__`\\   | (_) || | | || '__)/'_` | /'__`\\| '__)" << endl;
+    cout << "| |_/ /( (_| |\\__, \\| |_  | |  | ___/   | | | || |_| || |  ( (_| | | ___/| |   " << endl;
+    cout << "|____/'`\\__,_)(____/`\\__)(___)`\\____)   |_| |_|`\\___/'|_|  `\\__,_|`\\____)|_|   " << endl;
                                                                                
                                                                                
     // ASCII castle image
@@ -22,24 +23,24 @@ int main(int argc, char *argv)
     cout << "                           === " << endl;
     cout << "                          J|||F " << endl;
     cout << "                          F|||J " << endl;
-    cout << "                         /\/ \/\ " << endl;
+    cout << "                         /\\/ \\/\\ " << endl;
     cout << "                         F+++++J " << endl;
     cout << "                        J{}{|}{}F         . " << endl;
     cout << "                    .   F{}{|}{}J         T " << endl;
     cout << "          .         T  J{}{}|{}{}F       ;;;" << endl;
-    cout << "          T        /|\ F/\/\|/\/\J  .   ,;;;;. " << endl;
-    cout << "         /:\     //'/|\\:=========F T ./;;;;;;\ " << endl;
-    cout << "      ./:/:/.   ///|||\\\""""""""" /x\T\;;;;;;/  " << endl;
-    cout << "     //:/:/:/\  \\\\|////..[ ]...xXXXx..|====| " << endl;
-    cout << "     \:/:/:/:T7 :.:.:.:.:||[ ]|/xXXXXXx\|||||| " << endl;
-    cout << "     ::.:.:.:A. `;:;:;:;'=== ==\XXXXXXX/=====. " << endl;
-    cout << "     `;""::/ooo\.|,|,|,| ( ) ( )| | | |.=..=.| " << endl;
-    cout << "      :. :`\ooo/(_)(_)(_) _   _ | | | |'-''-'| " << endl;
-    cout << "      :T-'- :"":|""'''""|/ \ / \|=====|======| " << endl;
-    cout << "      .A.:::| |_||,. .. || | | |/\/\/\/ | | || " << endl;
-    cout << "   :;:////\:::.'.| || || ||-| |-|/\/\/\+|+| | | " << endl;
-    cout << "  ;:;;\////::::,='======='=============/\/\=====. " << endl;
-    cout << " :;:::;''':::::;:|__..,__|============/||\|\====| " << endl;
+    cout << "          T        /|\\ F/\\/\\|/\\/\\J  .   ,;;;;. " << endl;
+    cout << "         /:\\     //'/|\\:=========F T . /;;;;;;\\ " << endl;
+    cout << "      ./:/:/.   ///|||\\\\**********/xx\\T\\;;;;;;/  " << endl;
+    cout << "     //:/:/:/\\ \\ \\\|////..[ ]...xXXXx..|====| " << endl;
+    cout << "     \\:/:/:/:T7 :.:.:.:.:||[ ]|/xXXXXXx\\|||||| " << endl;
+    cout << "     ::.:.:.:A. `;:;:;:;'=== ==\\XXXXXXX/=====. " << endl;
+    cout << "     `;""::/ooo\\.|,|,|,| ( ) ( )| | | |.=..=.| " << endl;
+    cout << "      :. :`\ooo\/(_)(_)(_) _   _ | | | |'-''-'| " << endl;
+    cout << "      :T-'- :"":|""'''""|/ \\ / \\|=====|======| " << endl;
+    cout << "      .A.:::| |_||,. .. || | | |/\\/\\/\\/ | | || " << endl;
+    cout << "   :;:////\\:::.'.| || || ||-| |-|/\\/\\/\\+|+| | | " << endl;
+    cout << "  ;:;;\\////::::,='======='=============/\/\=====. " << endl;
+    cout << " :;:::;''':::::;:|__..,__|============/||\\|\\====| " << endl;
     cout << " :::::;|=:::;:;::|,;:::::          |========|   | " << endl;
     cout << " ::l42::::::(}:::::;::::::_________|========|___|__ " << endl;
     cout << endl << endl << endl;
@@ -70,38 +71,40 @@ int main(int argc, char *argv)
         cin >> playerChoice;
         if(playerChoice == '1')
         {
-            getmap();
-            cout << "Choose your action:" << endl;
-            cout << "\t1. Check Map" << endl;
-            cout << "\t2. Get current room information " << endl;
-            cout << "\t3. Interact with person" << endl;
-            cout << "\t4. Interact with item" << endl;
-            cout << "\t5. Go to a different room" << endl;
-            
+			getMap();           
         }
         else if(playerChoice == '2')
         {
-            //Lobby.getInfo();
+            getCurrentRoom();
+			result.getInfo();
         }
         else if(playerChoice == '3')
         {
-            
-        }
+            result.gettalk();
+		}
         else if(playerChoice == '4')
         {
-            
+            result.getitem();
         }
         else if(playerChoice == '5')
         {
-            
+			displayRoomlist();
+            cin >> playerChoice;
+			setCurrentRoom(playerChoice);
         }
         else
         {
             cout << "incorrect choice argument" << endl;
         }
+		cout << "Choose your action:" << endl;
+        cout << "\t1. Check Map" << endl;
+        cout << "\t2. Get current room information " << endl;
+        cout << "\t3. Interact with person" << endl;
+        cout << "\t4. Interact with item" << endl;
+        cout << "\t5. Go to a different room" << endl;
     }
 
 
-    return 0;
 
-}
+    return 0;
+	}
