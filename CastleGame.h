8fb4b650cce554e6be9509e2a351b2589fcfa04d
DownToNuggen <dn6634@hotmail.com>
2014-05-11 23:17:36 -0400

    Update CastleGame.h

--- CastleGame.h
+++ CastleGame.h
@@ -8,7 +8,7 @@
 	Room Room1("This is Icer's room, Icer is Always on Skype talking with his girlfriend or practicing dance routine for culture", "Room1", "Icer", "Sorry I was in a skype session with my girlfriend during the time of the murder.  We did hear a man screaming during our skype session which startled my girlfriend a little. My girlfriend never really liked Evan though.", "", "");
 	Room Room2("This is Dinh's room, Dinh is sleepy lazy gamer", "Room2", "Dinh", "Killer? I was sleeping when the murder happened because I usually stay up all night going around the room staring at random things until morning. The janitor really creeps me out though. He likes to walk around the hallways at night with his tattered coat.", "Condom", "A freshly used condom");
 	Room Room3("This is a empty room", "Room3", "", "", "", "");
-	Room BathRoom("This is bathroom, wait! why Xin is HERE?? He only speaks mandarin.", "BathRoom", "Xin", "他妈的，我是凶手。你要现在要不抓我，我就杀了你", "", "");
+	Room BathRoom("This is bathroom. there is a werid foreigner in here. He only speaks mandarin.", "Bathroom", "Xin", "他妈的，我是凶手。你要现在要不抓我，我就杀了你", "", "");
 	Room StudyRoom("This is study room, Ali is working on something..", "Study Room", "Ali", "Can't talk now, I'm on a strict deadline for this project. I'm building a timer to set off a delayed fire...for no evil means I swear.", "", "");
 	Room Kitchen("This is a big kicken, smells funny, no one is in here", "Kitchen", "", "", "", "");
 
