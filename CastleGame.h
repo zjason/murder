3241fa1f7f1b966827668860c1169c3bbfee2122
DownToNuggen <dn6634@hotmail.com>
2014-05-11 22:33:02 -0400

    Update CastleGame.h

--- CastleGame.h
+++ CastleGame.h
@@ -5,3 +5,47 @@ void initRoom()
 	
 	Room Lobby( "des", "Lobby", "Police officer", "Who is the murder", "item", "itemDes");
 }
+
+void getMap()
+{
+    //void(int currentmap)
+    if(currentmap == '1')
+    {
+    	cout <<"                                                     N        " << endl;
+    	cout <<"                                                     |        " << endl;
+    	cout <<"                                                 W---+---E    " << endl;
+    	cout <<"                                                     |        " << endl;
+    	cout <<"                                                     S        " << endl;
+    	cout <<"              {Base Stair}                                    " << endl;
+    	cout <<"              {          }                                    " << endl;
+    	cout <<"              {          }                                    " << endl;
+    	cout <<"              ~~~~~  ~~~~~                                    " << endl;
+    	cout <<"               ====  =====                         ========== " << endl;
+    	cout <<"              |           |====================== |          |" << endl;
+    	cout <<"              |           |           |           |          |" << endl;
+    	cout <<"              |           |  Room1    |   Room3   |          |" << endl;
+    	cout <<"              |           |   (1)     |   (3)     |          |" << endl;
+    	cout <<"              |    @@@    |   Icer    |===== =====|          |" << endl;
+    	cout <<"              |           |           |           |          |" << endl;
+    	cout <<"              |   Lobby    ==== ======|   Study      Kitchen |" << endl;
+    	cout <<"              |    (0)    First Hallway    Room       (6)    |" << endl;
+    	cout <<"              |            ==== ======|     (4)              |" << endl;
+    	cout <<"              |  Police   |           |   Alireza |          |" << endl;
+    	cout <<"              |           |  Room2    |===== ==== |          |" << endl;
+    	cout <<"              |           |   (2)     | BathRoom  |          |" << endl;
+    	cout <<"              |           |   Dinh    |   Xin (5) |          |" << endl;
+    	cout <<"               ==========  =========== ==========  ==========|" << endl;
+    	cout <<"              {          }                                    " << endl;
+    	cout <<"              {2nd  Stair}                                    " << endl;
+    	cout <<"              {          }                                    " << endl;
+    	cout <<"              ~~~~~~~~~~~~                                    " << endl;
+    }
+    //else if(currentmap == '2')
+    //{
+        
+    //}
+    //else
+    //{
+        
+    //}
+}
